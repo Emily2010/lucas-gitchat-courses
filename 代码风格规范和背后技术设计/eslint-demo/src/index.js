c79ef117e3ebec38e20e4654d3225ed138c99feb
houce <594965698@qq.com>
2019-03-23 20:45:26 +0800

    feat: add

---
+++ 代码风格规范和背后技术设计/eslint-demo/src/index.js
@@ -0,0 +1,16 @@
+let person = {
+	  		name: "Yoda",
+	designation: 'Jedi Master '
+	};
+	
+		 function trainJedi (jediWarrion) {
+	  if (jediWarrion.name === 'Yoda') {
+	    console.log('No need! already trained');
+	  }
+	console.log(`Training ${jediWarrion.name} complete`);
+	}
+	
+				trainJedi(person);
+	trainJedi({ name: 'Adeel',	designation: 'padawan'});
+
+let a;
