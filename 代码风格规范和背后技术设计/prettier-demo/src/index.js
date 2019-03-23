27666a9d2b10eea5ec4619a62bb3b017af70e449
houce <houce@zhihu.com>
2019-03-23 19:53:27 +0800

    feat: first commit

---
+++ 代码风格规范和背后技术设计/prettier-demo/src/index.js
@@ -0,0 +1,14 @@
+let person = {
+  name: 'Yoda',
+  designation: 'Jedi Master ',
+};
+
+function trainJedi(jediWarrion) {
+  if (jediWarrion.name === 'Yoda') {
+    console.log('No need! already trained');
+  }
+  console.log(`Training ${jediWarrion.name} complete`);
+}
+
+trainJedi(person);
+trainJedi({ name: 'Adeel', designation: 'padawan' });
